2be6041dcabd21f153bdd11d7486e6dfd6d5fbdf
Kiselev Danila <kdanila03062006@gmail.com>
2026-06-20 22:41:14 +0300

    Matchers - test 2

---
+++ __tests__/sortHeroes.test.js
@@ -0,0 +1,28 @@
+import sortHeroes from '../src/sortHeroes';
+
+test('should sort heroes by health descending', () => {
+  const heroes = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+
+  const sorted = sortHeroes(heroes);
+
+  expect(sorted).toEqual([
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ]);
+});
+
+test('should not mutate original array', () => {
+  const heroes = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+  ];
+
+  const sorted = sortHeroes(heroes);
+
+  expect(sorted).not.toBe(heroes);
+});
